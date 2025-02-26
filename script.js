ef2190c50ff63b8793b2d5a0c67a486b371b1216
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-26 13:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const colorInput = document.getElementById("colour_id");
+const blockInput = document.getElementById("block_id");
+const form = document.getElementById("colorForm");
+
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	const selectedColor=colorInput.value;
+	const blockNumber=blockInput.value.trim();
+	document.querySelectorAll(".grid-item").forEach(block => {
+        block.style.backgroundColor = "transparent";
+    });
+
+	if(blockNumber >=1 && blockNumber <=9){
+		const block=document.getElementById(`${blockNumber}`);
+		block.style.backgroundColor=selectedColor;
+	}
+	
+})
+
+
